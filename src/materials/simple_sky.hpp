2a5db5a4418f50b806b88deed0cf6aadf72356c4
Jacek Wieczorek <mrjjot@gmail.com>
2020-07-17 23:54:08 +0200

    Make sky black...

--- src/materials/simple_sky.hpp
+++ src/materials/simple_sky.hpp
@@ -16,7 +16,7 @@ public:
 		rt::ray_bounce bounce;
 		bounce.bsdf = glm::vec3{0.f};
 		// bounce.emission = glm::vec3{0.5, 0.5, 1.0} + 10.f * glm::vec3{std::pow(glm::dot(hit.direction, {0, 1, 0}), 4.f)}; 
-		bounce.emission = 0.25f * glm::mix(glm::vec3{0.3, 0.3, 0.4}, glm::vec3{1.1, 1.0, 2.0}, std::abs(hit.direction.y));
+		bounce.emission = 0.001f * glm::mix(glm::vec3{0.3, 0.3, 0.4}, glm::vec3{1.1, 1.0, 2.0}, std::abs(hit.direction.y));
 		return bounce;
 	}
 };
