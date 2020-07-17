8dbf26468e8b338bdec4d2ba4e8ba4ef6f1bd0ce
Jacek Wieczorek <mrjjot@gmail.com>
2020-07-17 23:58:39 +0200

    Import test scene instead of hard-coding one

--- src/rt.cpp
+++ src/rt.cpp
@@ -25,6 +25,7 @@
 #include "mesh_data.hpp"
 #include "aabb.hpp"
 #include "bvh_tree.hpp"
+#include "blender_jsd_loader.hpp"
 
 #include "materials/pbr_material.hpp"
 #include "materials/glass.hpp"
@@ -40,75 +41,7 @@ int main(int argc, char **argv)
 	glm::ivec2 render_size{1024, 1024};
 
 	// The scene
-	rt::scene scene;
-
-	// Test material
-	rt::pbr_material red_mat{{0.7, 0.1, 0.1}, 0.1};
-	rt::pbr_material better_red_mat{{0.37, 0.0, 0.0}, 0.05};
-	rt::pbr_material gold_mat{{0.8, 0.4, 0.1}, 0.2, 1.0};
-	rt::pbr_material green_mat{{0.1, 0.6, 0.1}, 0.5};
-	rt::pbr_material white_mat{{0.9, 0.9, 0.9}, 0.5};
-	rt::pbr_material glow_mat{{0.0f, 0.0f, 0.0f}, 1.0f, 0.0f, glm::vec3{25.f}};
-	rt::pbr_material mirror_mat{{0.8f, 0.8f, 0.8f}, 0.05f, 1.0f};
-	rt::simple_glass_material glass_mat{glm::vec3{0.8f, 0.042f, 0.161f}, 1.50f};
-
-	// Green sphere
-	rt::primitive_collection s2{rt::sphere{{3, 1, 1}, 1}};
-	rt::scene_object sphere2_obj{s2, green_mat};
-	scene.add_object(&sphere2_obj);
-
-	// Plane
-	rt::primitive_collection p{rt::plane{{0, 0, 0}, {0, 1, 0}}};
-	rt::scene_object plane_obj{p, white_mat};
-	scene.add_object(&plane_obj);
-
-	// Glowing sphere
-	rt::primitive_collection s3{rt::sphere{{1.5, 0.5, 3}, 0.5}};
-	rt::scene_object sphere3_obj{s3, glow_mat};
-	scene.add_object(&sphere3_obj);
-
-	// Mirror sphere
-	rt::primitive_collection s4{rt::sphere{{-1.5, 0.5, 3}, 0.5}};
-	rt::scene_object sphere4_obj{s4, mirror_mat};
-	scene.add_object(&sphere4_obj);
-	
-	// Better red sphere
-	rt::primitive_collection s5{rt::sphere{{5, 3, -4}, 3}};
-	rt::scene_object sphere5_obj{s5, better_red_mat};
-	scene.add_object(&sphere5_obj);
-
-	// Glass sphere
-	rt::primitive_collection s6{rt::sphere{{-2.f, 0.5, 0}, 0.5}};
-	rt::scene_object sphere6_obj{s6, better_red_mat};
-	scene.add_object(&sphere6_obj);
-
-	// Back wall
-	rt::plane wall_b{{0, 0, -8}, {0, 0, 1}};
-	rt::scene_object wall_b_obj{rt::primitive_collection{wall_b}, white_mat};
-	// scene.add_object(&wall_b_obj);
-
-	// Left wall
-	rt::plane wall_l{{-4, 0, 0}, {1, 0, 0}};
-	rt::scene_object wall_l_obj{rt::primitive_collection{wall_l}, green_mat};
-	// scene.add_object(&wall_l_obj);
-
-	// Right wall
-	rt::plane wall_r{{5, 0, 0}, {-1, 0, 0}};
-	rt::scene_object wall_r_obj{rt::primitive_collection{wall_r}, red_mat};
-	// scene.add_object(&wall_r_obj);
-
-	// Test mesh
-	rt::mesh_data monkey("resources/monkey.obj");
-	rt::primitive_collection monkey_pc{monkey};
-	rt::scene_object monkey_obj{monkey_pc, red_mat};
-	monkey_obj.set_transform(glm::translate(glm::vec3(1.f, 1.f, 0.f)));
-	scene.add_object(&monkey_obj);
-
-	rt::mesh_data bunny("resources/bunny.obj");
-	rt::primitive_collection bunny_pc{bunny};
-	rt::scene_object bunny_obj{bunny_pc, glass_mat};
-	bunny_obj.set_transform(glm::translate(glm::vec3(0.f, 0.f, 2.f)) * glm::scale(glm::vec3(2.f)));
-	scene.add_object(&bunny_obj);
+	rt::scene scene = rt::load_jsd_scene("resources/test_box.jsd");
 
 
 
@@ -120,7 +53,7 @@ int main(int argc, char **argv)
 	std::random_device rnd;
 
 	// Initial camera setup
-	rt::camera cam({0, 2, 6}, {0, 0, 1}, {0, 1, 0}, 0.01, glm::radians(60.f), 1.f);
+	rt::camera cam({12, 1, 0}, {0, 0, 1}, {0, 1, 0}, 0.01, glm::radians(60.f), 1.f);
 	cam.look_at({0, 0.5, 0});
 	scene.set_camera(cam);
 
